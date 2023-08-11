107fe69bb4b2af9a742bee5e6158d2910407e651
lhq <378715546@qq.com>
2023-08-11 17:30:38 +0800

    Changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -61,5 +61,5 @@ yarn install
 #read -p "Enter PORT: " PORT
 
 # Build and run the project using the environment variables
-OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn build
-OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn start
+#OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn build
+#OPENAI_API_KEY=$OPENAI_API_KEY CODE=$CODE PORT=$PORT yarn start
